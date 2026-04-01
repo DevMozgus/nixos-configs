1a25a69a117cc1f68facd5c3a4702df612e38070
DevMozgus <web@urlicic.dev>
2026-04-01 08:00:22 +0200

    set zsh as default

--- hosts/common/global/users.nix
+++ hosts/common/global/users.nix
@@ -1,11 +1,14 @@
 # User accounts, sudo, runtime password file
-{ ... }:
+{ pkgs, ... }:
 {
+  programs.zsh.enable = true;
+
   users.users.nicola = {
     isNormalUser = true;
     description = "Nicola";
     extraGroups = [ "wheel" "video" "audio" "networkmanager" "docker" ];
     hashedPasswordFile = "/persist/passwords/nicola";
+    shell = pkgs.zsh;
   };
 
   security.sudo.wheelNeedsPassword = true;
