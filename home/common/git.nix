7cb3fd1f34e11ecdabf37d955c029a6b83179796
Nicola <nicola@example.com>
2026-04-03 08:35:38 +0200

    fix git config

--- home/common/git.nix
+++ home/common/git.nix
@@ -4,22 +4,13 @@
   programs.git = {
     enable = true;
     settings = {
-      user.name = "Nicola";
-      user.email = "nicola@example.com";  # TODO: Set your real email
+      user.name = "DevMozgus";
+      user.email = "web@urlicic.dev";
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
-      alias = {
-        co = "checkout";
-        br = "branch";
-        ci = "commit";
-        st = "status";
-        lg = "log --oneline --graph --decorate";
-        unstage = "reset HEAD --";
-        last = "log -1 HEAD";
-      };
     };
   };
 
