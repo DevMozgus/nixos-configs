a831aab420ee7a310b8798cbf43817a66f5f975e
DevMozgus <web@urlicic.dev>
2026-04-13 13:46:55 +0200

    Fix beads-viewer URL: map NixOS arch to GitHub arch
    GitHub releases use amd64/arm64, but NixOS uses x86_64/aarch64.
    Add arch mapping to construct correct download URLs.

--- pkgs/beads-viewer/default.nix
+++ pkgs/beads-viewer/default.nix
@@ -9,8 +9,16 @@ stdenv.mkDerivation rec {
   pname = "beads-viewer";
   version = "0.15.2";
 
+  arch =
+    if stdenv.hostPlatform.parsed.cpu.name == "x86_64" then
+      "amd64"
+    else if stdenv.hostPlatform.parsed.cpu.name == "aarch64" then
+      "arm64"
+    else
+      stdenv.hostPlatform.parsed.cpu.name;
+
   src = fetchzip {
-    url = "https://github.com/Dicklesworthstone/beads_viewer/releases/download/v${version}/bv_${version}_linux_${stdenv.hostPlatform.parsed.cpu.name}.tar.gz";
+    url = "https://github.com/Dicklesworthstone/beads_viewer/releases/download/v${version}/bv_${version}_linux_${arch}.tar.gz";
     sha256 =
       if stdenv.hostPlatform.parsed.cpu.name == "x86_64" then
         "467c7dee72c599e915d638eb22335a91eb842171eddc2e7baf43129058a7664e"
