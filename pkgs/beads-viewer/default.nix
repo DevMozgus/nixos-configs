cb3aa30afca627c480edf0f0afa50a118caebf50
DevMozgus <web@urlicic.dev>
2026-04-13 13:57:11 +0200

    Change from fetchzip to fetchurl to fix hash issues
    fetchzip hashes unpacked contents which was causing mismatch.
    fetchurl hashes the downloaded tarball directly.

--- pkgs/beads-viewer/default.nix
+++ pkgs/beads-viewer/default.nix
@@ -1,7 +1,7 @@
 {
   stdenv,
   lib,
-  fetchzip,
+  fetchurl,
   ...
 }:
 
@@ -17,19 +17,22 @@ stdenv.mkDerivation rec {
     else
       stdenv.hostPlatform.parsed.cpu.name;
 
-  src = fetchzip {
+  src = fetchurl {
     url = "https://github.com/Dicklesworthstone/beads_viewer/releases/download/v${version}/bv_${version}_linux_${arch}.tar.gz";
     sha256 =
       if stdenv.hostPlatform.parsed.cpu.name == "x86_64" then
         "0kk6lxc904j3mxxjxp7df4hq9swib8rj5srqsqayk6f5fbp7sz26"
       else
         "1gj1fjs6df2i0ipvlc05wj5cq5s0lwmkcif4an5v0m6xrqkv7jab";
-    stripRoot = false;
   };
 
   dontConfigure = true;
   dontBuild = true;
   dontPatchELF = true;
+
+  unpackPhase = ''
+    tar -xzf $src
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
